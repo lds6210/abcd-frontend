5cf6a7cfd9b832d9d79b149f606662d2525c860f
dongsub.lee <lds6210@naver.com>
2022-06-14 01:44:24 +0900

    2강

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,29 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+  <div>
+    {{logo}}
+    <h4> XX 원룸 </h4>
+    <p> {{price1}}만원 </p>
+  </div>
+  <div>
+    <h3> {{products[0]}} </h3>
+    <h4> XX 원룸 </h4>
+    <p> {{price2}} 만원 </p>
+  </div>
 </template>
-
+  
+  
 <script>
 export default {
   name: 'App',
+  data(){
+    return {
+      price1 : 60,
+      price2 : 80,
+      logo : "燮雲",
+      products : ['1','2','3'],
+    }
+  },
   components: {
     
   }
